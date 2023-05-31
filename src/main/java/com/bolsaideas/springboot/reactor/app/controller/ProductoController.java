9a033284768b5568d3656a45ae8b91921d2321ae
DavidRivera <0333171040@unjfsc.edu.pe>
2023-05-31 16:40:00 -0500

    Put and delete mapping

--- src/main/java/com/bolsaideas/springboot/reactor/app/controller/ProductoController.java
+++ src/main/java/com/bolsaideas/springboot/reactor/app/controller/ProductoController.java
@@ -4,10 +4,7 @@ import com.bolsaideas.springboot.reactor.app.models.dao.ProductoDao;
 import com.bolsaideas.springboot.reactor.app.models.documents.Producto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -39,5 +36,26 @@ public class ProductoController {
                 .doOnNext(prod -> log.info(prod.getNombre()));
 
         return producto;
+    }
+
+    @PutMapping("/{id}")
+    public Mono<Producto> update(@RequestBody Producto producto, @PathVariable String id){
+        return dao.findById(id)
+                .flatMap(p -> {
+                    p.setNombre(producto.getNombre());
+                    p.setPrecio(producto.getPrecio());
+                    return dao.save(p);
+                });
+    }
+
+    @PostMapping
+    public Mono<Producto> save(@RequestBody Producto producto){
+        return dao.save(producto);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> delete(@PathVariable String id){
+        return dao.findById(id)
+                .flatMap(p -> dao.delete(p));
     }
 }
